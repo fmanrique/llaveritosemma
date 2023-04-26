7ae3bc003231fa591547575f31d38715891bda06
Francisco Manrique <paco@djnr.net>
2023-04-26 09:16:25 -0600

    Add share image

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,3 @@
 export const EXAMPLE_PATH = 'blog-starter'
 export const CMS_NAME = 'LLaveritos Emma'
-export const HOME_OG_IMAGE_URL = '/assets/llaveritos-share.jpg'
+export const HOME_OG_IMAGE_URL = 'https://llaveritosemma.com/assets/llaveritos-share.jpg'
