5c8eba172701dbaa53b9bc8ec1860bc0812cc80d
Francisco Manrique <paco@djnr.net>
2023-04-26 09:22:33 -0600

    Fix title

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,3 @@
 export const EXAMPLE_PATH = 'blog-starter'
-export const CMS_NAME = 'LLaveritos Emma'
+export const CMS_NAME = 'Llaveritos Emma'
 export const HOME_OG_IMAGE_URL = 'https://llaveritosemma.com/assets/llaveritos-share.jpg'
